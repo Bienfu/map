9b01e00b4b1bbd08d52029c5bd3b42e85f3c16ae
Bienfu <bairdd@oregonstate.edu>
2020-07-24 15:26:32 -0700

    basic map functions implemented with list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import MapContainer from "./Map";
+import MyMapComponent from "./MapAlt";
+import "./App.css";
+
+const repairShops = [
+  {
+    name: "Repair Shop 1",
+    address: "Address 1",
+    position: {
+      lat: 37.787519,
+      lng: -122.41,
+    },
+  },
+  {
+    name: "Repair Shop 2",
+    address: "Address 2",
+    position: {
+      lat: 37.75,
+      lng: -122.42,
+    },
+  },
+  {
+    name: "Repair Shop 3",
+    address: "Address 3",
+    position: {
+      lat: 37.797,
+      lng: -122.40564,
+    },
+  },
+];
+
+function ShopList(props) {
+  const shops = props.repairShops;
+  const handleClick = (evt) => {
+    const shopName = evt.currentTarget.dataset.name;
+    const shop = shops.find(x => x.name === shopName);
+    return props.onClickFunction(shop)
+  };
+  const listItems = shops.map((shop) => (
+    <li key={shop.name}>
+      <button className={(shop==props.activeShop) ? "active" : null} onClick={handleClick} data-name={shop.name}>
+        {shop.name}
+        <br />
+        {shop.address}
+      </button>
+    </li>
+  ));
+  return <ul>{listItems}</ul>;
+}
+
+function Display({activeShop:{name, address}}) {
+  // const {activeShop:{name, address}} = props;
+  // const {name, address} = activeShop; 
+  return (
+    <div>{name} {address}</div>
+  );
+}
 
 function App() {
+  const [activeShop, setShop] = useState({
+    name: "Test Shop",
+    address: "Test Address",
+    position: {
+      lat: 37.778519,
+      lng: -122.40564,
+    },
+  });
+
+  const setActiveShop = (newShop) => setShop(newShop);
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +84,23 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <body>
+        <div class="content">
+          <MapContainer 
+            repairShops={repairShops}
+            activeShop={activeShop}
+            onClickFunction={setActiveShop}
+          ></MapContainer>
+          <ShopList
+            onClickFunction={setActiveShop}
+            activeShop={activeShop}
+            repairShops={repairShops}
+          ></ShopList>
+          <Display activeShop={activeShop}/>
+          {/* <MyMapComponent></MyMapComponent> */}
+        </div>
+      </body>
     </div>
   );
 }
---
+++ src/Map.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+export class MapContainer extends Component {
+  render() {
+    const containerStyle = {
+      position: "relative",
+      width: "500px",
+      height: "500px",
+    };
+    const shops = this.props.repairShops;
+    const mapCenter = this.props.activeShop.position;
+    const handleClick = (evt) => {
+      const shopName = evt.name;
+      const shop = shops.find(x => x.name === shopName);
+      return this.props.onClickFunction(shop)
+    };
+    const listItems = shops.map((shop) =>
+    <Marker key={shop.name}  name={shop.name} position={shop.position} onClick={handleClick}>
+    </Marker>
+  );
+    return (
+      <Map  google={this.props.google} containerStyle={containerStyle} intitialCenter={{lat: 37.778519, lng: -122.405640}} center={mapCenter} style={containerStyle} zoom={14}>
+        {/* <Marker onClick={this.onMarkerClick} name={"Current location"} /> */}
+        {listItems}
+        {/* <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow> */}
+      </Map>
+    );
+  }
+}
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyBHKQHB3WXPUS7z7bCnlNHhA-isVof3sSk",
+  v: "3.30",
+})(MapContainer);
